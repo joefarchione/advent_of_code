5521d048b95605e281f112f0312b6705432d4956
Joe Farchione <jfarchione@gmail.com>
2024-12-31 11:41:12 -0600

    5

--- advent2024/lib/day05.ml
+++ advent2024/lib/day05.ml
@@ -1,102 +1,109 @@
 open Core
 
-module IntSet = Set.Make(Int)
+module Page = struct type t = int [@@deriving sexp, equal, compare] end
+module PageSet = Set.Make(Page)
 
-module OrderingRules = struct
-  module M = Map.Make(Int)
+module PageRulesBefore = struct
+  module M = Map.Make(Page)
   include M
 
-  let from_tuples (pairs: (int*int) list) = 
-    let rec aux (pairs: (int*int) list) map = 
-      match pairs with 
-      | (a,b)::tl -> (
-        let map = Map.update map a ~f:(fun v_opt -> 
-            match v_opt with
-            | Some (v) -> (Set.add v b)
-            | None -> (Set.add IntSet.empty b)
-        )
-        in
-        aux tl map
+  type t = PageSet.t M.t
+
+  let from_lines lines = 
+    let rec from_lines' lines mapping = 
+      match lines with 
+      | (a, b)::tl -> (
+        let mapping = Map.update mapping a ~f:(function | Some (v) -> (Set.add v b) | None -> (Set.add PageSet.empty b)) in 
+        from_lines' tl mapping
       )
-      | [] -> map
+      | [] -> mapping
     in
-    let rules = M.empty in 
-    aux pairs rules
+    from_lines' lines empty
 
-  let check map before after  = 
-    let a = match Map.find map before with 
-    | Some (pages) -> (Set.mem pages after)
-    | None -> true  in 
-
-    let b = match Map.find map after with 
-    | Some (pages) -> not (Set.mem pages before)
-    | None -> true  in 
-
-    a && b
-
+  let can_be_before t page page_before = 
+    match Map.find t page with 
+    | None -> true
+    | Some (pages) -> not (Set.mem pages page_before)
 end
 
-module PageList = struct
 
-  type t = int list
+module PageOrdering = struct 
+  type t = Page.t list [@@deriving sexp, equal, compare] 
+  let get_middle_exn t = 
+    let length = (List.length t - 1) in 
+    let middle = if length % 2 = 0 then length / 2 else length / 2 + 1 in 
+    List.nth_exn t middle
 
-  let rec check_ordering rules pages = 
-    match pages with 
-    | hd :: tl -> 
-      if List.for_all tl ~f:(OrderingRules.check rules hd) then
-        check_ordering rules tl
-      else 
-        false
+  let rec check_rule rules (pages_before: t) (page: Page.t) = 
+    match pages_before with 
+    | page_before::other_pages when PageRulesBefore.can_be_before rules page page_before -> 
+      check_rule rules other_pages page
+    | _::_ -> false
     | [] -> true
 
-  let get_middle pages = 
-    match List.nth pages ((List.length pages) / 2) with 
-    | Some (v) -> v
-    | None -> failwith "incorrect index?"
-
 end
 
-let pages = let open Re in seq [rep1 digit] |> compile
-let rule = let open Re in seq [bol; group (rep1 digit); str "|"; group (rep1 digit); eol] |> compile 
+type input = {rules: PageRulesBefore.t; orderings: PageOrdering.t list}
 
-let find_rules line = 
-    let open Re in 
-    let matched =  exec_opt rule line in 
-    match matched with 
-    | Some (m) -> ((Int.of_string (Group.get m 1)), (Int.of_string (Group.get m 2)))
-    | None -> failwith "not a rule!"
-
-let find_pages line = 
-  Re.matches pages line 
-  |> List.map ~f:Int.of_string
-
-let parse_lines lines = 
-  let rec aux lines parsing_pages rules pages =
-    match lines with 
-    | [] -> (OrderingRules.from_tuples rules, pages)
-    | hd :: tl -> 
-      if (String.equal hd "\n") || (String.equal hd "") then 
-        aux tl true rules pages
-      else if parsing_pages then (
-        let pages = (find_pages hd) :: pages in 
-        aux tl parsing_pages rules pages
-      )
-      else (
-        let rules = (find_rules hd) :: rules in
-        aux tl parsing_pages rules pages
-      )
-  in
-  aux lines false [] []
-
-let solve_p1 filepath = 
+let read filepath =
+  let re_digits = (Re.rep1 Re.digit |> Re.compile) in 
   In_channel.read_lines filepath
-  |> parse_lines
-  |> fun (rules, page_lists) -> (
-    List.filter page_lists ~f:(PageList.check_ordering rules) 
-    |> List.map ~f:PageList.get_middle
-    |> List.fold_left ~f:(fun acc a -> acc + a) ~init:0
-    |> Printf.printf "%d";
+  |> Utils.split_on_newline 
+  |> (fun split -> 
+    match split with
+    | [lines_rules;lines_orderings] -> (
+      let rules = List.map lines_rules 
+        ~f:(fun line -> (
+            match (Re.matches re_digits line) with 
+            | [a;b] -> (int_of_string a, int_of_string b)
+            | _ -> failwith "incorrect input"
+          )
+        )
+        |> PageRulesBefore.from_lines
+      in 
+      let orderings = List.map lines_orderings
+        ~f:(fun line -> 
+          match Re.matches re_digits line with 
+          | [] -> failwith "incorrect input"
+          | lst -> (List.map lst ~f:int_of_string)
+        )
+      in 
+      {rules; orderings}
+    )
+    | _ -> failwith "Incorrect input"
   )
 
 
+let check_ordering rules (ordering: PageOrdering.t) = 
+  let rec aux rules (ordering: PageOrdering.t)  = 
+    match ordering with 
+    | hd::tl when PageOrdering.check_rule rules tl hd -> aux rules tl
+    | _::_ -> false
+    | [] -> true
+    in 
+    aux rules (List.rev ordering)
 
+let check_ordering_move rules (ordering: PageOrdering.t) = 
+  let rec aux rules (ordering: PageOrdering.t) new_ordering swapped  = 
+    match ordering with 
+    | hd::tl when PageOrdering.check_rule rules tl hd -> aux rules tl (hd::new_ordering) swapped
+    | a::b::tl -> aux rules ((b::tl)@[a]) new_ordering true
+    | a::[] when swapped -> Some (a::new_ordering) 
+    | [] when swapped -> Some new_ordering
+    | _ -> None
+    in 
+    aux rules (List.rev ordering) [] false 
+
+let solve_p1 input = 
+  input.orderings 
+  |> List.filter ~f:(check_ordering input.rules)
+  |> List.map ~f:PageOrdering.get_middle_exn
+  |> List.fold ~init:0 ~f:(+)
+  |> Printf.printf "%d"
+
+let solve_p2 input = 
+  input.orderings 
+  |> List.filter_map ~f:(check_ordering_move input.rules)
+  |> List.map ~f:PageOrdering.get_middle_exn
+  |> List.fold ~init:0 ~f:(+)
+  |> Printf.printf "%d"
