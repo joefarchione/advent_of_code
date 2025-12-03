627c8050595d58ae1aae44d1d656689ab06293fc
Joe Farchione <jfarchione@gmail.com>
2025-12-03 10:00:08 -0500

    update main

--- advent2025/bin/main.ml
+++ advent2025/bin/main.ml
@@ -1,11 +1,17 @@
 open Core
 open Advent2025_lib
 
+let solve day_str solve1 solve2 = 
+  let input_dir = "/home/joefarchione/Projects/advent_of_code/advent2025/inputs" in
+  let filepath = Filename.concat input_dir (Printf.sprintf "day%s.txt" day_str) in
+  printf "Day%s: (" day_str;
+  solve1 filepath;
+  printf ", ";
+  solve2 filepath;
+  printf ")\n"
+
+
 let () =
-  let advent (input_dir: string) = 
-    printf "Day 01 - part 1: "; (Day01.solve1 (Filename.concat input_dir "day01.txt")); printf "\n";
-    printf "Day 01 - part 2: "; (Day01.solve2 (Filename.concat input_dir "day01.txt")); printf "\n";
-    printf "Day 02 - part 1: "; (Day02.solve1 (Filename.concat input_dir "day02.txt")); printf "\n";
-    printf "Day 02 - part 2: "; (Day02.solve2 (Filename.concat input_dir "day02.txt")); printf "\n";
-  in
-  advent "/home/joefarchione/Projects/advent_of_code/advent2025/inputs"
+  solve "01" Day01.solve1 Day01.solve2;
+  solve "02" Day02.solve1 Day02.solve2;
+  solve "03" Day03.solve1 Day03.solve2;
