10b3054af0804c6c94dafd7b6353518318778b7d
Joe Farchione <jfarchione@gmail.com>
2025-12-11 12:16:54 -0500

    day 11

---
+++ advent2025/lib/day11.ml
@@ -0,0 +1,39 @@
+open Aoc
+
+let read input =
+  input |> String.split_lines
+  |> List.map ~f:(fun l ->
+         let open Re in
+         let pat = alpha |> rep1 |> compile in
+         let strings = Re.all pat l |> List.map ~f:(fun g -> Group.get g 0) in
+         (List.hd_exn strings, List.tl_exn strings))
+  |> Map.of_alist_exn (module String)
+
+module Key = struct
+  type t = string * string * int [@@deriving sexp, hash, compare]
+end
+
+let paths_a_to_b ?(req_nodes = Set.empty (module String)) a b cnxns =
+  let inc_req_count count node =
+    if Set.mem req_nodes node || Set.is_empty req_nodes then count + 1
+    else count
+  in
+  let aux =
+    Memo.memo_rec
+      (module Key)
+      (fun aux (a, b, req_count) ->
+        if String.equal a b then if req_count >= 2 then 1 else 0
+        else
+          let required = inc_req_count req_count a in
+          Map.find_exn cnxns a
+          |> List.fold ~init:0 ~f:(fun acc v -> acc + aux (v, b, required)))
+  in
+  aux (a, b, 0)
+
+let solve1 input = read input |> paths_a_to_b "you" "out"
+
+let solve2 input =
+  read input
+  |> paths_a_to_b
+       ~req_nodes:(Set.of_list (module String) [ "fft"; "dac" ])
+       "svr" "out"
--- advent2025/lib/utils/memo.ml
+++ advent2025/lib/utils/memo.ml
@@ -10,14 +10,14 @@ let memo f =
         m := (x, y) :: !m;
         y
 
-let memo_rec f =
-  let m = ref [] in
-  let rec g x =
-    match List.Assoc.find !m ~equal:Poly.equal x with
+let memo_rec module_ f =
+  let table = Hashtbl.create module_ in
+  let rec rec_f x =
+    match Hashtbl.find table x with
     | Some v -> v
     | None ->
-        let y = f g x in
-        m := (x, y) :: !m;
-        y
+        let v = f rec_f x in
+        Hashtbl.set table ~key:x ~data:v;
+        v
   in
-  g
+  rec_f
